e1ecdcc07cdf9ad645693f6cafdc5670e49cb6a3
Selenebm <126086029+Selenebm@users.noreply.github.com>
2024-11-11 14:21:34 -0500

    cuarto cambio: Verificacion de disponibilidad de horarios, ademas agregue más medicos

--- app/agenda.py
+++ app/agenda.py
@@ -1,4 +1,5 @@
 from cita import Cita
+
 class Agenda:
     def __init__(self, id_agenda: int, medico):
         self.id_agenda = id_agenda
@@ -6,13 +7,23 @@ class Agenda:
         self.citas = []
 
     def agregar_cita(self, cita: Cita):
+        # Verificar si el horario de la cita está disponible
+        if cita.hora not in self.medico.horarios_disponibles:
+            raise ValueError(f"El horario {cita.hora} no está disponible para el médico {self.medico.nombre}.")
+
+        # Agregar cita a la agenda si el horario está disponible
         self.citas.append(cita)
+        self.actualizar_disponibilidad(cita.hora)
 
     def eliminar_cita(self, cita: Cita):
         self.citas.remove(cita)
 
     def consultar_disponibilidad(self):
+        # Devuelve los horarios disponibles del médico
         return self.medico.horarios_disponibles
 
-    def fabricar_cita(self, medico, paciente, fecha, hora):
-        return Cita.crear_cita(medico, paciente, fecha, hora)
+    def actualizar_disponibilidad(self, hora: str):
+        # Actualiza la lista de horarios disponibles eliminando el horario agendado
+        if hora in self.medico.horarios_disponibles:
+            self.medico.horarios_disponibles.remove(hora)
+
